21d40650cf58cac6a929e4d17297980d0be1d095
Eric Swanson <eric.ca.swanson@aa.com>
2019-07-03 18:47:25 -0400

    fix: browser timeout

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
+        browserNoActivityTimeout: 60000,
         browsers: ['ChromeHeadlessNoSandbox', 'Chrome'],
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
