d0147ed64e00a2099d89089fbead69e89b0f4786
Eric Swanson <eric.ca.swanson@aa.com>
2019-07-05 15:23:44 -0400

    fix: e2e page title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('new App', () => {
         page = new AppPage();
     });
 
-    it('should be blank', () => {
+    it('should have title', () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toContain('The world is your oyster.');
+        expect(page.getTitle()).toContain('Our Technology Architecture');
     });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
         return browser.get('/');
     }
 
-    getParagraphText() {
-        return element(by.deepCss('app-root ion-content')).getText();
+    getTitle() {
+        return element(by.deepCss('app-root ion-content ion-card-title')).getText();
     }
 }
