2b4cdd39775bc20bd68b36128bc6a15d98a5a09a
Eric Swanson <eric.ca.swanson@aa.com>
2019-07-26 15:40:51 -0400

    feat: #17 register page views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -18,16 +20,50 @@ export class AppComponent implements OnInit {
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
+        private router: Router,
         private pwa: PwaService,
         private toaster: ToastController,
+        private title: Title,
     ) {
         this.initializeApp();
     }
 
     initializeApp() {
+
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+        });
+
+        this.router.events.subscribe((event) => {
+
+            if (event instanceof NavigationEnd) {
+
+                const navEndEvent = event as NavigationEnd;
+
+                const currentPageTitle = this.title.getTitle();
+
+                let pageTitle = currentPageTitle;
+
+                const titleElements = document.getElementsByTagName('ion-title');
+
+                if (titleElements.length > 0) {
+                    pageTitle = titleElements[0].textContent;
+                    this.title.setTitle(pageTitle);
+                }
+
+                const pageViewData = {
+                    page_title: pageTitle,
+                    page_path: navEndEvent.urlAfterRedirects,
+                };
+
+                console.log('Sending page view to GA', pageViewData);
+
+                (window as any).gtag('config', 'UA-144592331-1', {
+                    page_title: pageTitle,
+                    page_path: navEndEvent.urlAfterRedirects,
+                });
+            }
         });
     }
 
