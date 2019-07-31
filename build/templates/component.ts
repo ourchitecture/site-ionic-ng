6def284c8876d749c5398146fb75be6a7d3856c2
Eric Swanson <eric.ca.swanson@aa.com>
2019-07-31 16:19:55 -0400

    feat: #18 patreon

--- build/templates/component.ts
+++ build/templates/component.ts
@@ -13,6 +13,9 @@ import { Component } from '@angular/core';
             <a routerLink="/">Ourchitecture</a>
         </ion-title>
         <ion-buttons slot="end">
+            <ion-chip color="primary">
+                <ion-label><a href="https://www.patreon.com/bePatron?u=22778259">Become a Patron!</a></ion-label>
+            </ion-chip>
             <ion-button fill="solid" href="https://github.com/ourchitecture">
                 <ion-icon slot="end" name="logo-github"></ion-icon>
                 GitHub
